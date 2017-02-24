bacb444793fd0a9d072edc85ca8f77375bfee591
yangxt <yxtxiaotian@163.com>
2017-02-24 15:54:35 +0800

    规范函数库接口命名

--- 3des/test.c
+++ 3des/test.c
@@ -1,22 +1,23 @@
-#include "des.h"
+#include "libtripledes.h"
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
     char key[16];
+	//memcpy(key,"\xDC\x32\x07\x1C\x1F\xBC\xF7\x0D\x67\xA8\x0B\x6E\x34\x49\x98\x51",16);
 	memcpy(key,"\x01\x09\x08\x00\x09\x01\x07\x04\x01\x09\x09\x01\x00\x07\x02\x08",16);
     char str[8] = "hello";
 
     printf("before encrypt...\n");
     printf("str = %s\n", str);
-    TripleDes(str, str, key, 1); 
-    //des(str, key, 1);
+    libTripleDes(str, str, key, 1); 
+    //libDes(str, key, 1);
     printf("after encrypt...\n");
     printf("str = %s\n", str);
 
-    TripleDes(str, str, key, 0); 
-    //des(str, key, 0);
+    libTripleDes(str, str, key, 0); 
+    //libDes(str, key, 0);
     printf("after decrypt...\n");
     printf("str = %s\n", str);
     return 0;
